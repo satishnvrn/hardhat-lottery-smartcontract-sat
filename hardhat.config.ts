7de332343e22fd4037a392e8829eed2a0cb55a41
satishnvrn <nvrn.satish@gmail.com>
2023-07-09 16:28:27 +0530

    Added hardhat framework

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.18" }, { version: "0.6.6" }]
+  },
+};
+
+export default config;
